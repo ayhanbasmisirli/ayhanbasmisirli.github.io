a73d2acae7da3b786f901ecefafaf0220791a15d
Ayhan Basmisirli <ayhan.basmisirli@gmail.com>
2024-02-18 01:22:48 +0300

    pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
-      <div className="w-full max-w-[600px]">
-        <h1>O</h1>
-      </div>
+      <div className="w-full max-w-[600px]"></div>
     </div>
   );
 }
