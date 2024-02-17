725d2e7e2d98be2f105d785c63601ea0a9621fef
Ayhan Basmisirli <ayhan.basmisirli@gmail.com>
2024-02-18 02:51:38 +0300

    pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
-      <div className="w-full max-w-[600px]"></div>
+    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* IMAGE CONTAINER */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+        <Image src="/hero.png" alt="" fill className="object-contain" />
+      </div>
+      {/* TEXT CONTAINER */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+        {/* TITLE */}
+        <h1 className="text-4xl md:text-6xl font-bold">
+          Crafting Digital Experiences, Designing Tomorrow.
+        </h1>
+        {/* DESC */}
+        <p className="md:text-xl">
+          Welcome to my digital canvas, where innovation and creativity
+          converge. With a keen eye for aesthetics and a mastery of code, my
+          portfolio showcases a diverse collection of projects that reflect my
+          commitment to excellence.
+        </p>
+        {/* BUTTONS */}
+      </div>
     </div>
   );
 }
