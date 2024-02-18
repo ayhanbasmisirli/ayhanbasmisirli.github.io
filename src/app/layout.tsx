661c41e4ecc6673d68fe9f613405adb3f0bb1704
Ayhan Basmisirli <ayhan.basmisirli@gmail.com>
2024-02-18 12:43:47 +0300

    fill information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import TransitionProvider from "./components/transitionProvider";
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,6 +17,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-42501330-3"
+        />
+
+        <Script id="google-analytics">
+          {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', UA-42501330-3"});
+  `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <TransitionProvider>{children}</TransitionProvider>
       </body>
