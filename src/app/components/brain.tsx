abb88d0cbd9ed30bcacda6ae5a003a807180f951
Ayhan Basmisirli <ayhan.basmisirli@gmail.com>
2024-02-18 03:01:41 +0300

    pages

--- src/app/components/brain.tsx
+++ src/app/components/brain.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import { motion, useTransform } from "framer-motion";
 
-const Brain = ({ scrollYProgress }) => {
+const Brain = ({ scrollYProgress }: any) => {
   const rotatesForward1 = useTransform(scrollYProgress, [0, 1], [0, 360]);
   const rotatesForward2 = useTransform(scrollYProgress, [0, 1], [0, 180]);
   const rotatesForward3 = useTransform(scrollYProgress, [0, 1], [0, 90]);
--- src/app/components/navlink.tsx
+++ src/app/components/navlink.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const NavLink = ({ link }) => {
+const NavLink = ({ link }: any) => {
   const pathName = usePathname();
 
   return (
